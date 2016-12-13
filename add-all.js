aadc9488130fcf94fe816e26519140288b477685
二楼西 <deffyc@139.com>
2016-12-13 17:38:15 +0800

    update add-all.js

---
+++ add-all.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+var readline = require('readline');
+var fs = require('fs');
+var path = require('path');
+var INFO = "./data/info.json";
+
+var keyArry = ['name', 'description', 'url', 'tags'];
+var fReadName = 'data/all.txt';
+var fRead = fs.createReadStream(fReadName); 
+var objReadline = readline.createInterface({
+    input: fRead,
+});
+/*var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});*/
+
+var i = 0;
+var rst = {};
+
+var DATAPATH = './data/sites/';
+var info = JSON.parse(fs.readFileSync('./data/info.json', 'utf-8'));
+
+function init() {
+	objReadline.on('line', (line)=>{
+		str=line.replace(/(^\s*)|(\s*$)/g,"");
+		var reg=new RegExp("^#");
+		if(!reg.test(str) && str != ""){
+			rst[keyArry[i]] = str;
+			i++;
+
+			if (i == keyArry.length) {
+				fs.writeFile(path.join(DATAPATH, (++info.totalCount) + '.json'), JSON.stringify({
+					name : rst.name,
+					id : info.totalCount++,
+					description: rst.description,
+					tags: rst.tags,
+					url: rst.url,
+					createAt: +new Date()
+				}), function(err) {
+					if (err) {
+						throw err;
+					}
+				});
+
+				i = 0;
+				rst = {};
+			}
+		}
+	});  
+}
+function writeSiteInfo(info) {
+    fs.writeFileSync(INFO, JSON.stringify(info), 'utf8');
+    console.log('基本信息生成完成');
+}
+init();
